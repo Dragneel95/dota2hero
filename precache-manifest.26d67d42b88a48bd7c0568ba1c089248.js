7f0539ea639ba8b3252e3771ea48c6d35118e7aa
Zaki Shaikh <zaki.shaikh.o2o@gmail.com>
2019-08-17 16:41:51 +0530

    Updates

---
+++ precache-manifest.26d67d42b88a48bd7c0568ba1c089248.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73f8be904c14bed940e97c7863266512",
+    "url": "/herolist/index.html"
+  },
+  {
+    "revision": "59d49a8330ebc98f25b7",
+    "url": "/herolist/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "917ec21f226f0594452c",
+    "url": "/herolist/static/css/main.64583935.chunk.css"
+  },
+  {
+    "revision": "59d49a8330ebc98f25b7",
+    "url": "/herolist/static/js/2.bae3d14d.chunk.js"
+  },
+  {
+    "revision": "917ec21f226f0594452c",
+    "url": "/herolist/static/js/main.0090a0ee.chunk.js"
+  },
+  {
+    "revision": "0aa00b4922ad009dfec6",
+    "url": "/herolist/static/js/runtime~main.eb9dd724.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/herolist/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "b98712d4c4e0bdc64fb503fb0d67bcd6",
+    "url": "/herolist/static/media/background.b98712d4.jpg"
+  }
+]);
